fd53df96cc1abf8989b71a261ccae062d8af1a32
choeSarah <121065007+choeSarah@users.noreply.github.com>
2024-04-08 19:52:50 -0600

    Added multiple pages

--- educationApp/mainwindow.cpp
+++ educationApp/mainwindow.cpp
@@ -6,9 +6,73 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QComboBox *pageComboBox = new QComboBox;
+
+    pageComboBox->addItem(tr("alphabet"));
+    pageComboBox->addItem(tr("vocabulary"));
+    pageComboBox->addItem(tr("game"));
+
+    connect(pageComboBox,
+            &QComboBox::activated,
+            ui->stackedWidget,
+            &QStackedWidget::setCurrentIndex);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_alphabet_navBtn1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_alphabet_navBtn2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+
+}
+
+void MainWindow::on_alphabet_navBtn3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_vocabulary_navBtn1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_vocabulary_navBtn3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_vocabulary_navBtn2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_game_navBtn1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_game_navBtn3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_game_navBtn2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
--- educationApp/mainwindow.h
+++ educationApp/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QComboBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -13,9 +14,29 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+public slots:
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+
+    void on_alphabet_navBtn2_clicked();
+    void on_alphabet_navBtn1_clicked();
+    void on_alphabet_navBtn3_clicked();
+
+    void on_vocabulary_navBtn1_clicked();
+
+    void on_vocabulary_navBtn3_clicked();
+
+    void on_vocabulary_navBtn2_clicked();
+
+    void on_game_navBtn1_clicked();
+
+    void on_game_navBtn3_clicked();
+
+    void on_game_navBtn2_clicked();
 
 private:
     Ui::MainWindow *ui;
